69b24f1ea2993501641612ad90bcb30fe82a829e
Pavel Hunko <pavelhunko@gmail.com>
2015-03-24 17:51:43 +0300

    test

--- gen/com/nutritionfit/R.java
+++ gen/com/nutritionfit/R.java
@@ -26,10 +26,10 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080008;
-        public static final int tableRow1=0x7f080000;
         public static final int tableRow2=0x7f080002;
         public static final int tableRow3=0x7f080004;
         public static final int tableRow4=0x7f080006;
+        public static final int tableRowCurID=0x7f080000;
         public static final int user_activity_level=0x7f080005;
         public static final int user_current_weight=0x7f080001;
         public static final int user_target_weight=0x7f080003;
